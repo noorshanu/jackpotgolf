4c954799eeca19102972a113b8353789953e1180
noor alam <51040978+noorshanu@users.noreply.github.com>
2026-05-06 15:02:56 +0530

    sd

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import Perfectshot from "./components/Perfectshot";
 import Precesion from "./components/Precesion";
 import MasterApproch from "./components/MasterApproch";
 import GolfLove from "./components/GolfLove";
+import Membership from "./components/Membership";
 
 export default function Home() {
   return (
@@ -15,6 +16,7 @@ export default function Home() {
         <Precesion />
         <MasterApproch />
         <GolfLove />
+        <Membership />
       </main>
     </>
   );
